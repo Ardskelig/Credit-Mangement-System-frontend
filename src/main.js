d6107e9943fb9b6de7dce7f365725ea8e4f498a4
Ardskelig <152053547+Ardskelig@users.noreply.github.com>
2024-12-18 19:49:18 +0800

    修改了表格展示的内容，改变了学生对象的结构，增加了echart表格

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
+import ECharts from 'vue-echarts'
+import 'echarts'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(ElementPlus)
+app.component('ECharts',ECharts)
 app.mount('#app')
